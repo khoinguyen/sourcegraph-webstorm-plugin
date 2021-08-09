d21b2ceb6a4336886272a11464bf197067217ea5
GitHub Action <action@github.com>
2021-08-09 14:07:48 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/khoinguyen/sourcegraphwebstormplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.khoinguyen.sourcegraphwebstormplugin.services
+
+import com.github.khoinguyen.sourcegraphwebstormplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
